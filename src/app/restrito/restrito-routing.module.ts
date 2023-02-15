9f613a81bcf9b042ce8bd440080f8dd8d607e25b
Guilherme Rigaud <guirigaud@hotmail.com>
2023-02-15 16:13:45 -0300

    Meu primeiro site com consumo de API

---
+++ src/app/restrito/restrito-routing.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from 'src/app/inicio/inicio.component';
+import { LoginComponent } from 'src/app/login/login.component';
+import { RestritoComponent } from 'src/app/restrito/restrito.component';
+import { AtualizaProdutoComponent } from './atualiza-produto/atualiza-produto.component';
+import { CadastroProdutoComponent } from './cadastro-produto/cadastro-produto.component';
+import { ListaProdutoComponent } from './lista-produto/lista-produto.component';
+import { GuardGuard } from '../guard.guard';
+
+const routes: Routes = [
+    { path: 'inicio', component: InicioComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'restrito', component: RestritoComponent, children: [
+            {
+                path: 'cadastro', component: CadastroProdutoComponent, canActivate:
+                    [GuardGuard]
+            },
+            {
+                path: 'lista', component: ListaProdutoComponent, canActivate:
+                    [GuardGuard]
+            },
+            {
+                path: 'editar/:id', component: AtualizaProdutoComponent, canActivate:
+                    [GuardGuard]
+            }
+        ]
+    },
+    {path: '', redirectTo: '/restrito/lista', pathMatch:'full'}
+]
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+
+export class RestritoRoutingModule { }
